28a718050df31a2ef307741d819b00f6bfd940a9
袁军贤 <jxyuan@dunoinfo.com>
2019-06-26 16:53:47 +0800

    1、添加弹框的title显示 2、添加报警弹框组件

--- src/components/popupinfo/index.js
+++ src/components/popupinfo/index.js
@@ -1,16 +1,27 @@
-import popupinfo from "./popupinfo.vue"
-export default popupinfo
+export { default as popupinfo } from './popupinfo.vue' // 实时信息、历史告警、历史数据
+export { default as popupOneInfo } from './popuponeinfo.vue' // 单个告警信息，带复位、保存功能
 
 /* 
-  <!-- 使用示例 -->
+  <!-- 一 -->
   <popupinfo :visible="visible" @onClose="visible = !visible"/>
+  <!-- 二 -->
+  <popupOneInfo :visible="visible" title="4号主变01#枪机" :itemData="itemData" imgSrc="http://pic44.nipic.com/20140719/2531170_081420875000_2.jpg" @onClose="visible = !visible" />
 
-  import popupinfo from '_c/popupinfo'
+
+  import { popupinfo, popupOneInfo } from '_c/popupinfo'
   export default {
-    components: {popupinfo},
+    components: {popupinfo, popupOneInfo},
     data() {
       return {
-        visible: false
+        visible: false,
+        itemData: {
+          title: '4号主变01#枪机',
+          tepm: '98℃',
+          threshold: '61%',
+          defect: '危机缺陷',
+          time: '05-21 13:33:23',
+          location: '此处显示设备具体方位'
+        }
       }
     },
     methods: {
---
+++ src/components/popupinfo/popuponeinfo.vue
@@ -0,0 +1,116 @@
+<template>
+  <historical-documents width="400px" :title="title" :isShowTab="false" :dialogTableVisible="visible" @close="onClose">
+    <div class="popuponeinfo">
+      <el-row>
+        <el-col :span="12"><p class="itemTitle">当前温度：<span>{{itemData.tepm}}</span></p></el-col>
+        <el-col :span="12"><p class="itemTitle">超出阈值：<span>{{itemData.threshold}}</span></p></el-col>
+        <el-col :span="24"><p class="itemTitle">缺陷评估：<span class="alarm">危险缺陷</span></p></el-col>
+      </el-row>
+      <div>
+        <div class="imgBox">
+          <img :src="imgSrc">
+          <p class="itemTitle itemBottomTitle">位置：{{itemData.location}}</p>
+        </div>
+      </div>
+      <el-row>
+        <el-col :span="15"><h5 class="itemTitle time">{{itemData.time}}</h5></el-col>
+        <el-col :span="9"><div class="buttonAll"><el-button type="info" round @click="restoration()">复位</el-button><el-button type="success" round @click="retain()">保存</el-button></div></el-col>
+      </el-row>
+    </div>
+  </historical-documents>
+</template>
+<script>
+import HistoricalDocuments from '_c/duno-c/HistoricalDocuments'
+export default {
+  name: 'popuponeinfo',
+  components: {HistoricalDocuments},
+  props: {
+    title: {
+      type: String,
+      default: () => {
+        return ''
+      }
+    },
+    imgSrc: {
+      type: String,
+      default: () => {
+        return ''
+      }
+    },
+    itemData: {
+      type: Object,
+      default: () => {
+        return {}
+      }
+    },
+    visible: {
+      type: Boolean,
+      default: () => {
+        return false
+      }
+    }
+  },
+  methods: {
+    restoration () {
+      console.log('复位')
+    },
+    retain () {
+      console.log('保存')
+    },
+    onClose (data) {
+      this.$emit('onClose', data)
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+  .popuponeinfo {
+    .itemTitle {
+      color: #fff;
+      font-size: 18px;
+      padding: 5px 0;
+      .alarm {
+        color: #d7203f;
+        font-size: 22px;
+      }
+      .warning {
+        color: #ff8300;
+        font-size: 22px;
+      }
+      .general {
+        color: #ffe828;
+        font-size: 22px;
+      }
+    }
+    .itemBottomTitle {
+      font-size: 16px !important;
+      padding: 5px 10px;
+      background: rgba(0,0,0,0.5);
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      overflow: hidden;
+      text-overflow:ellipsis;
+      white-space: nowrap;
+    }
+    .time {
+      font-size: 16px;
+    }
+    .imgBox {
+      width: 100%;
+      height: 0;
+      margin: 20px auto;
+      padding-bottom: 100%;
+      position: relative;
+      img {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+      }
+    }
+    .buttonAll {
+      text-align: right;
+    }
+  }
+</style>
