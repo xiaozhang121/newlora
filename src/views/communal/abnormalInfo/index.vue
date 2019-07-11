e7029c5550d86bc914049dda62016eae4d446a06
袁军贤 <jxyuan@dunoinfo.com>
2019-07-11 20:54:43 +0800

    修改异常信息弹框的传值

--- src/views/communal/abnormalInfo/index.vue
+++ src/views/communal/abnormalInfo/index.vue
@@ -69,7 +69,7 @@
       />
     </duno-main>
     <warning-setting @handleClose="onClose" :visibleOption="visibleSettingOption" />
-    <wraning :visible="visible" warningID="20190711002" :alarmFileAddress="popData.alarmFileAddress || ''" :judgeResult="popData.alarmContent || ''" :origin="popData.monitorDeviceId" :handleResult="popData.dealRecord || ''" @handleClose="handleClose" />
+    <wraning :visible="visible" warningID="20190711002" :monitorUrl="popData.alarmFileAddress || ''" :judgeResult="popData.alarmContent || ''" :origin="popData.monitorDeviceId" :handleResult="popData.dealRecord || ''" @handleClose="handleClose" />
   </div>
 </template>
 
