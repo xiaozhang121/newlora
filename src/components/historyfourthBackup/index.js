aba9146ae35f858575d2622496f90b1c5502d959
myn <mao_allen@foxmail.com>
2019-07-11 23:00:20 +0800

    假数据弹窗

---
+++ src/components/historyfourthBackup/index.js
@@ -0,0 +1,17 @@
+import historyfourthBackup from './historyfourth.vue'
+export default historyfourthBackup
+/* 
+<historyfourth title="云台可见光-TGTYS" v-if="cameraFlag == 'sixth'"/>
+
+import historyfourth from "_c/historyfourth"
+
+export default {
+  components: {historyfourth},
+  data() {
+    return {
+      cameraFlag: ''
+    }
+  },
+  methods: {}
+}
+*/
