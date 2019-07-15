4ec439f1459acc7193055f1b749cb2f95c6c4109
曹勇 <ycao@dunoinfo.com>
2019-07-15 10:21:01 +0800

    数据分析

---
+++ src/views/communal/alarmInfo/analysis.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="analysis">
+    <div class="breadcrumb">
+      <Breadcrumb />
+    </div>
+    <div class="dunoDrap">
+      <div>重点监测区域</div>
+      <div class="selectBtn">
+        <div>
+          <duno-btn-top
+            @on-select="onSelectAmmeter"
+            class="dunoBtnTop"
+            :isCheck="false"
+            :dataList="ammeterData"
+            :title="titleAmmeter"
+            :showBtnList="false"
+          ></duno-btn-top>
+        </div>
+        <div>
+          <duno-btn-top
+            @on-select="onSelectParts"
+            class="dunoBtnTop"
+            :isCheck="false"
+            :dataList="partsData"
+            :title="titleParts"
+            :showBtnList="false"
+          ></duno-btn-top>
+        </div>
+        <div>
+          <duno-btn-top
+            @on-select="onSelectPhase"
+            class="dunoBtnTop"
+            :isCheck="false"
+            :dataList="phaseData"
+            :title="titlePhase"
+            :showBtnList="false"
+          ></duno-btn-top>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "analysis",
+  data() {
+    return {
+      titleAmmeter: "",
+      titleParts: "",
+      titlePhase: "",
+      ammeterData: [],
+      partsData: [],
+      phaseData: []
+    };
+  },
+  methods: {
+    onSelectAmmeter(item) {
+      this.titleAmmeter = item["describeName"];
+    },
+    onSelectParts(item) {
+      this.titleParts = item["describeName"];
+    },
+    onSelectPhase(item) {
+      this.titlePhase = item["describeName"];
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
