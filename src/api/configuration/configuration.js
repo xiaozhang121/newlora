eaf5baa62606fcf9f22c8deb8e4d1ea9f466d195
曹勇 <ycao@dunoinfo.com>
2019-07-17 20:40:58 +0800

    接口

--- src/api/configuration/configuration.js
+++ src/api/configuration/configuration.js
@@ -48,6 +48,23 @@ export const lightNewInformation = (data) => {
     })
 }
 
+//设备监测-红外监测-最新巡检报告
+export const infraNewReport = (data) => {
+    return axios.request({
+        url: "/lenovo-plan/api/plan/iir-report/list",
+        method: 'get'
+    })
+}
+
+//设备监测-红外监测-最新24小时记录信息
+export const infraNewInformation = (data) => {
+    // debugger
+    return axios.request({
+        url: "/lenovo-plan/api/task/iir-result/list",
+        method: 'get'
+    })
+}
+
 //所有报表-表计分析-echarts
 export const getEchartsData = (data) => {
     return axios.request({
---
+++ src/views/communal/equipment/components/MonitorWarn.vue
@@ -0,0 +1,207 @@
+<template>
+  <div class="alarmLog">
+    <div class="img">
+      <KeyMonitor
+        streamAddr="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"
+        :imgAdress="remarkData.alarmFileAddress"
+      />
+    </div>
+    <div class="content">
+      <div class="top">
+        <p>
+          监测对象:
+          <span>{{remarkData.powerDeviceName}}</span>
+        </p>
+      </div>
+      <div class="main">
+        <div>
+          部位:
+          <span>{{remarkData.part}}</span>
+        </div>
+        <div>
+          内容:
+          <span>{{remarkData.content}}</span>
+        </div>
+        <div>
+          <i>记录:</i>
+          <p>
+            <span v-for="(item,index) in dealRecord.slice(0,2)" :key="index">{{item}}</span>
+          </p>
+        </div>
+      </div>
+      <div class="btn">
+        <p v-if="isShow">
+          拍摄来源:
+          <span>{{remarkData.monitorDeviceId}}</span>
+          <i @click="clickRemarks">备注</i>
+          <i @click="addReturn" :disabled="isDisabled">复归</i>
+        </p>
+        <p v-else>
+          拍摄来源:
+          <span>{{remarkData.monitorDeviceId}}</span>
+          <i>查看详情></i>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import moment from "moment";
+import KeyMonitor from "_c/duno-c/KeyMonitor";
+import { dealRemarks } from "@/api/configuration/configuration.js";
+export default {
+  name: "AlarmLog",
+  components: { KeyMonitor },
+  props: {
+    isShow: {
+      type: Boolean,
+      default: () => {
+        return true;
+      }
+    },
+    remarkData: {
+      type: Object,
+      default: () => {
+        return {};
+      }
+    },
+    content: {
+      type: String
+    }
+  },
+  data() {
+    return {
+      address: "",
+      isDisabled: true,
+      dealRecord: []
+    };
+  },
+  methods: {
+    addReturn() {
+      const that = this;
+      let query = {
+        alarmId: that.remarkData.alarmId,
+        type: "1"
+      };
+      dealRemarks(query).then(res => {
+        if (res.data.isSuccess) that.$message.success(res.msg);
+        else that.$message.error(res.msg);
+      });
+      this.$emit("handleListData");
+      //   if (this.isDisabled) {
+      //     let strTime =
+      //       "复归 (" + moment(new Date()).format("YYYY-MM-DD HH:mm:ss") + ")";
+      //     this.dealRecord.unshift(strTime);
+      //     this.isDisabled = false;
+      //   }
+    },
+    clickRemarks() {
+      const that = this;
+      let query = {
+        alarmId: that.remarkData.alarmId,
+        type: "2",
+        content: that.content
+      };
+      dealRemarks(query).then(res => {
+        if (res.data.isSuccess) that.$message.success(res.msg);
+        else that.$message.error(res.msg);
+      });
+      this.$emit("handleListData");
+    }
+  },
+  mounted() {
+    if (!this.remarkData.dealRecord) {
+      return;
+    }
+    this.remarkData.dealRecord.forEach(el => {
+      let str = el.dealRecord + " (" + el.dealTime + ")";
+      this.dealRecord.push(str);
+    });
+  }
+};
+</script>
+
+
+<style lang="scss">
+.alarmLog {
+  float: left;
+  margin-bottom: 20px;
+  width: calc(50% - 10px);
+  margin-left: 20px;
+  height: 180px;
+  background-color: #203644;
+  border-radius: 2px;
+  display: flex;
+  justify-content: space-around;
+  .img {
+    width: 40%;
+    height: 100%;
+    .keyMonitor {
+      width: 100% !important;
+    }
+  }
+  .content {
+    width: 60%;
+    padding: 10px 20px;
+    .top {
+      p {
+        font-size: 18px;
+        color: #ffffff;
+        span {
+          padding-left: 5px;
+        }
+      }
+    }
+    .main {
+      margin-top: 5px;
+      div {
+        font-size: 14px;
+        color: #ffffff;
+        display: flex;
+        justify-content: flex-start;
+        & > span {
+          padding-left: 10px;
+        }
+        i {
+          font-style: normal;
+        }
+        p {
+          padding-left: 10px;
+          span {
+            display: block;
+          }
+        }
+      }
+    }
+    .btn {
+      margin-top: 15px;
+      line-height: 32px;
+      p {
+        font-size: 14px;
+        color: #ffffff;
+        span {
+          cursor: pointer;
+          color: #3baddf;
+          padding-left: 5px;
+        }
+        i {
+          cursor: pointer;
+          float: right;
+          font-style: normal;
+          display: block;
+          //   width: 68px;
+          padding: 0 20px;
+          background-color: #3a81a1;
+          border-radius: 16px;
+          margin-left: 10px;
+          text-align: center;
+        }
+        i:last-child {
+          background-color: #305e83;
+        }
+      }
+    }
+  }
+}
+</style>
