865fdc269e09cea6bde8df3aaae13d6a62dcf04f
allenn <allenn@qq.com>
2019-07-29 12:03:19 +0800

    地址修改

--- src/router/index.js
+++ src/router/index.js
@@ -7,72 +7,72 @@ import { getToken, canTurnTo } from '@/libs/util'
 Vue.use(Router)
 
 const router = new Router({
-  mode: 'history',
-  routes
+    mode: 'history',
+    routes
 })
 const LOGIN_PAGE_NAME = 'login'
 
 router.beforeEach((to, from, next) => {
-  if('kind' in to.meta){
-      store.state.app.kilovolt = to.meta.kind
-  }
-  if(to.name.indexOf('abnormalInfoList')>-1){
-      store.state.app.topNav = 1
-  }else if(to.name.indexOf('realEnv')>-1){
-      store.state.app.topNav = 2
-  }else if(to.name.indexOf('surveillanceList')>-1){
-      store.state.app.topNav = 3
-  }
-  let token = getToken()
-  if (!token && to.name !== LOGIN_PAGE_NAME && !to.meta.isLogin) {
-    // 未登录且要跳转的页面不是登录页
-    next({
-      name: LOGIN_PAGE_NAME // 跳转到登录页
-    })
-  } else if ((!token && to.name === LOGIN_PAGE_NAME) || to.meta.isLogin) {
-    // 未登陆且要跳转的页面是登录页
-    next() // 跳转
-  } else if (token && to.name === LOGIN_PAGE_NAME) {
-    // 已登录且要跳转的页面是登录页
-    next({
-      name: 'realEnvList' // 跳转到home页
-    })
-  } else {
-    if (store.state.user.userId) {
-      if (canTurnTo(to.name, store.state.user.access, routes)) {
-        if (to.name == 'ticketList') {
-          window.location.href = 'http://52.82.107.5:8080/ar/cardManage'
-        } else if (to.name == 'inspectionList') {
-          window.location.href = 'http://52.82.107.5:8080/ar/taskManage'
-        } else if (to.name == 'inspectionList') {
-          window.location.href = 'http://52.82.107.5:8080/video/list'
-        } else {
-          next() // 有权限，可访问
-        }
-      } else next({ replace: true, name: 'error_401' }) // 无权限，重定向到401页面
-    } else {
-      store.dispatch('getUserInfo').then(user => {
-        store.dispatch('setConfigure').then(config=>{
-            // 拉取用户信息，通过用户权限和跳转的页面的name来判断是否有权限访问;access必须是一个数组，如：['super_admin'] ['super_admin', 'admin']
-            if (canTurnTo(to.name, user.access, routes)) {
-              if (to.name == 'ticketList') {
-                window.location.href = 'http://52.82.107.5:8080/ar/cardManage'
-              } else if (to.name == 'inspectionList') {
-                window.location.href = 'http://52.82.107.5:8080/ar/taskManage'
-              } else if (to.name == 'inspectionList') {
-                window.location.href = 'http://52.82.107.5:8080/video/list'
-              } else {
-                next() // 有权限，可访问
-              }
-            } else next({ replace: true, name: 'error_401' }) // 无权限，重定向到401页面
-        })
-      })
+    if('kind' in to.meta){
+        store.state.app.kilovolt = to.meta.kind
     }
-  }
+    if(to.name.indexOf('abnormalInfoList')>-1){
+        store.state.app.topNav = 1
+    }else if(to.name.indexOf('realEnv')>-1){
+        store.state.app.topNav = 2
+    }else if(to.name.indexOf('surveillanceList')>-1){
+        store.state.app.topNav = 3
+    }
+    let token = getToken()
+    if (!token && to.name !== LOGIN_PAGE_NAME && !to.meta.isLogin) {
+        // 未登录且要跳转的页面不是登录页
+        next({
+            name: LOGIN_PAGE_NAME // 跳转到登录页
+        })
+    } else if ((!token && to.name === LOGIN_PAGE_NAME) || to.meta.isLogin) {
+        // 未登陆且要跳转的页面是登录页
+        next() // 跳转
+    } else if (token && to.name === LOGIN_PAGE_NAME) {
+        // 已登录且要跳转的页面是登录页
+        next({
+            name: 'realEnvList' // 跳转到home页
+        })
+    } else {
+        if (store.state.user.userId) {
+            if (canTurnTo(to.name, store.state.user.access, routes)) {
+                if (to.name == 'ticketList') {
+                    window.location.href = 'http://10.0.10.35:8080/ar/cardManage'
+                } else if (to.name == 'inspectionList') {
+                    window.location.href = 'http://10.0.10.35:8080/ar/taskManage'
+                } else if (to.name == 'inspectionList') {
+                    window.location.href = 'http://10.0.10.35:8080/video/list'
+                } else {
+                    next() // 有权限，可访问
+                }
+            } else next({ replace: true, name: 'error_401' }) // 无权限，重定向到401页面
+        } else {
+            store.dispatch('getUserInfo').then(user => {
+                store.dispatch('setConfigure').then(config=>{
+                    // 拉取用户信息，通过用户权限和跳转的页面的name来判断是否有权限访问;access必须是一个数组，如：['super_admin'] ['super_admin', 'admin']
+                    if (canTurnTo(to.name, user.access, routes)) {
+                        if (to.name == 'ticketList') {
+                            window.location.href = 'http://10.0.10.35:8080/ar/cardManage'
+                        } else if (to.name == 'inspectionList') {
+                            window.location.href = 'http://10.0.10.35:8080/ar/taskManage'
+                        } else if (to.name == 'inspectionList') {
+                            window.location.href = 'http://10.0.10.35:8080/video/list'
+                        } else {
+                            next() // 有权限，可访问
+                        }
+                    } else next({ replace: true, name: 'error_401' }) // 无权限，重定向到401页面
+                })
+            })
+        }
+    }
 })
 
 router.afterEach(to => {
-  // window.scrollTo(0, 0)
+    // window.scrollTo(0, 0)
 })
 
 export default router
