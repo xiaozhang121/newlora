6f71694f91b1e03aee5c350845aa96ac42e378ea
cxycom <cxymm22@gmail.com>
2019-08-03 16:54:31 +0800

    隐藏截图 录像

--- src/components/duno-m/hotCameraB/hotCamera.vue
+++ src/components/duno-m/hotCameraB/hotCamera.vue
@@ -8,8 +8,8 @@
                             <video-player  ref="videoPlayer" class="vjs-custom-skin" :options="playerOptions"></video-player>
                         </div>
                         <div class="explain iconList" style="margin-top: 4px">
-                            <span><i class="iconfont icon-luxiang"></i>录像</span>
-                            <span><i class="iconfont icon-jietu"></i>截图</span>
+                            <!-- <span><i class="iconfont icon-luxiang"></i>录像</span>
+                            <span><i class="iconfont icon-jietu"></i>截图</span> -->
                             <span @click="fullScreen('videoPlayer')"><i class="iconfont icon-quanping"></i>全屏</span>
                         </div>
                     </div>
@@ -18,8 +18,8 @@
                             <video-player  ref="videoPlayerd" class="vjs-custom-skin" :options="playerOptiond"></video-player>
                         </div>
                         <div class="explain iconList" style="bottom: 10px; position: absolute;">
-                            <span><i class="iconfont icon-luxiang"></i>录像</span>
-                            <span><i class="iconfont icon-jietu"></i>截图</span>
+                            <!-- <span><i class="iconfont icon-luxiang"></i>录像</span>
+                            <span><i class="iconfont icon-jietu"></i>截图</span> -->
                             <span @click="fullScreen('videoPlayerd')"><i class="iconfont icon-quanping"></i>全屏</span>
                         </div>
                     </div>
