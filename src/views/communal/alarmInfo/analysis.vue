357ca849e69eb21baad251230aa90b3d778b9a82
曹勇 <ycao@dunoinfo.com>
2019-08-28 20:46:24 +0800

    图表

--- src/views/communal/alarmInfo/analysis.vue
+++ src/views/communal/alarmInfo/analysis.vue
@@ -666,7 +666,6 @@ export default {
     },
     clearChart() {
       let that = this;
-      debugger;
       that.legendOption.data = [];
       that.seriesOption = [];
       that.xAxisOption.data = [];
@@ -730,13 +729,11 @@ export default {
       };
       getEchartsData(query).then(res => {
         let that = this;
-        // if (res.data[0] == null) {
-        //   that.legendOption.data = [];
-        //   that.seriesOption = [];
-        //   that.xAxisOption.data = [];
-        //   return;
-        // }
         const dataList = res.data;
+        if (dataList == null) {
+          this.clearChart();
+          return;
+        }
         const legendData = [];
         const xAxisData = [];
         const seriesData = [];
@@ -746,10 +743,6 @@ export default {
         that.seriesOption = [];
         that.xAxisOption.data = [];
         for (let i = 0; i < dataList.length; i++) {
-          if (!dataList[i].deviceName) {
-            this.clearChart();
-            return;
-          }
           legendData.push(dataList[i].deviceName);
           const itemDataList = dataList[i].dataList;
           yMax.push(Number(dataList[i].maxValue));
