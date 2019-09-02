3be35dd6058e5bea1bcc37188bea7fefd673b906
曹勇 <ycao@dunoinfo.com>
2019-09-02 17:48:53 +0800

    转Number类型

--- src/components/duno-c/patrolDetail/patrolDetail.vue
+++ src/components/duno-c/patrolDetail/patrolDetail.vue
@@ -152,7 +152,7 @@ export default {
       };
       getAxiosData(url, query).then(res => {
         this.dataList = res.data;
-        this.totalRows = res.data.pageParam.totalRows;
+        this.totalRows = Number(res.data.pageParam.totalRows);
         console.log("121212", this.totalRows);
         this.loadingOption = false;
         this.$forceUpdate();
