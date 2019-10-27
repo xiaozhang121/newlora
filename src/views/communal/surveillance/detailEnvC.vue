f3218f77b51f71f45c7ab956e8e86fc2bf63dcac
myn <myn@qq.com>
2019-10-27 16:11:48 +0800

    样式修复

--- src/views/communal/surveillance/detailEnvC.vue
+++ src/views/communal/surveillance/detailEnvC.vue
@@ -1689,6 +1689,12 @@ export default {
       }
     }
   }
+  .control_slider{
+    top: 105% !important;
+  }
+  .cameraSpeed{
+    bottom: -25% !important;
+  }
 }
 .el-popper[x-placement^="bottom"] {
   background: #192f41;
