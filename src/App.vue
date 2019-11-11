84da1dc7ab943ce40f839bb7576d36abba44d648
myn <mao_allen@foxmail.com>
2019-11-11 15:25:56 +0800

    弹出层级问题修复

--- src/App.vue
+++ src/App.vue
@@ -164,6 +164,9 @@ export default {
   @import "./assets/icons/iconfont.css";  // 项目引用图标库
   @import "./style/index.scss";
   /*@import "@/style/noselect.scss";*/
+  .ivu-tooltip-popper{
+    z-index: 9999 !important;
+  }
   .el-loading-mask{
     z-index: 0 !important;
   }
