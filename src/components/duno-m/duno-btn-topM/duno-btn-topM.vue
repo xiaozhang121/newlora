643ec8d6dfe035bbc1f77430ea0e468ed542f6bb
曹勇 <ycao@dunoinfo.com>
2019-11-16 10:43:45 +0800

    滚动条

--- src/components/duno-m/duno-btn-topM/duno-btn-topM.vue
+++ src/components/duno-m/duno-btn-topM/duno-btn-topM.vue
@@ -749,4 +749,21 @@
       }
     }
   }
+  .btn_main::-webkit-scrollbar {
+    /*滚动条整体样式*/
+    width: 5px; /*高宽分别对应横竖滚动条的尺寸*/
+    height: 1px;
+  }
+  .btn_main::-webkit-scrollbar-thumb {
+    /*滚动条里面小方块*/
+    border-radius: 20px;
+    box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
+    background: rgba(255, 255, 255, 0.5);
+  }
+  .btn_main::-webkit-scrollbar-track {
+    /*滚动条里面轨道*/
+    box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
+    border-radius: 10px;
+    background: rgba(0, 0, 0, 0);
+  }
 </style>
