d0285673b99c8b7f74d8600a75f961baa4712d7b
myn <mao_allen@foxmail.com>
2019-12-11 10:48:49 +0800

    增加模块

---
+++ src/views/communal/security/frontDoor.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="frontDoor">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "frontDoor",
+    components: {},
+    data() {
+      return {};
+    },
+    watch: {
+      $route(to) {
+
+      },
+      routeName(now) {
+
+      }
+    },
+    methods: {},
+    mounted() {
+
+    },
+    beforeDestroy() {
+
+    }
+  };
+</script>
+
+<style lang="scss">
+  .frontDoor {
+
+  }
+</style>
+
