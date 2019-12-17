268f48811a155f68e7abfea2d806e8f94c037405
myn <mao_allen@foxmail.com>
2019-12-17 11:35:04 +0800

    去除机器人截图

--- src/views/communal/robotF/indexIN.vue
+++ src/views/communal/robotF/indexIN.vue
@@ -22,6 +22,7 @@
                     :pushCamera="false"
                     :monitorInfo="{'monitorDeviceId': 12}"
                     :isLive='false'
+                    :isRecord="false"
                     :isNav='true'
                     :isAux='true'
                     :isRobot='true'
