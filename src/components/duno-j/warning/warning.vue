7e687fc59783b1e3c97304100614f06443f276d7
myn <mao_allen@foxmail.com>
2019-12-30 16:47:17 +0800

    问题修复

--- src/components/duno-j/warning/warning.vue
+++ src/components/duno-j/warning/warning.vue
@@ -60,7 +60,7 @@
                 <p
                   class="monitorTitle"
                   :style="{color:(dataList.alarmContent || dataList.result || dataList.alarmValue || '').indexOf('正常')>-1?'#333':'red'}"
-                >{{dataList.alarmContent?dataList.alarmContent:(dataList.result?dataList.result:'')}}<br v-if="(dataList.alarmContent?dataList.alarmContent:(dataList.result?dataList.result:'')) && !(hasSelect && popData['alarmLevel'] && dataList.result !='正常')"/><span v-if="!(hasSelect && popData['alarmLevel'] && dataList.result !='正常')">{{ isNaN(Number(dataList.alarmValue))?dataList.alarmValue:dataList.alarmValue+'℃' }}</span></p>
+                >{{dataList.alarmContent?dataList.alarmContent:(dataList.result?dataList.result:'')}}<br v-if="(dataList.alarmContent?dataList.alarmContent:(dataList.result?dataList.result:'')) && !(hasSelect && popData['alarmLevel'] && dataList.result !='正常')"/><span v-if="!(hasSelect && popData['alarmLevel'] && dataList.result !='正常')">{{ isNaN(Number(dataList.alarmValue))?dataList.alarmValue:dataList.alarmValue+dataList.unit }}</span></p>
                 <p v-if="hasSelect && popData['alarmLevel'] && dataList.result !='正常'">
                   {{dataList.alarmValue?dataList.alarmValue:dataList.result}}
                   {{ (dataList.alarmContent||dataList.alarmContent)=='红外温度超过阈值'?'℃':'' }}
